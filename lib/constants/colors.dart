e2516d9e1f98c3a141aa258babb996fc54339e37
Khaleb165 <calebagyei165@gmail.com>
2023-11-29 16:51:42 +0000

    add new colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,5 +1,6 @@
-import'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
 Color primaryColor = Colors.greenAccent;
-Color shade1 = Color.fromRGBO(0, 7, 240,0.5);
-Color shade2 = Color.fromRGBO(127,0,255,100);
+Color primary2 = Color.fromRGBO(19, 41, 75, 1);
+Color shade1 = Color.fromRGBO(0, 7, 240, 0.5);
+Color shade2 = Color.fromRGBO(127, 0, 255, 100);
