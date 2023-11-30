7eb93ce4ee5227413d6c30317c026a074d349d06
Khaleb165 <calebagyei165@gmail.com>
2023-11-30 13:01:51 +0000

    change primaryColor to primary1

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
-Color primaryColor = Colors.greenAccent;
+Color primary1 = Colors.greenAccent;
 Color primary2 = Color.fromRGBO(19, 41, 75, 1);
 Color shade1 = Color.fromRGBO(0, 7, 240, 0.5);
 Color shade2 = Color.fromRGBO(127, 0, 255, 100);
+Color colorWhite = Colors.white;
