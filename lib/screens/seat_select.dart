d39c576c1eb90dd22f21b75f839232394c2d7eaa
Khaleb165 <calebagyei165@gmail.com>
2024-01-01 12:42:28 +0000

    reformatting code to make it clean

--- lib/screens/seat_select.dart
+++ lib/screens/seat_select.dart
@@ -81,14 +81,16 @@ class _SeatSelectPageState extends State<SeatSelectPage> {
   Widget build(BuildContext context) {
     final double totalPrice = selectedSeats.length * 80;
     return Scaffold(
-        // resizeToAvoidBottomInset: false,
-        backgroundColor: Colors.deepPurple[50],
-        body: SingleChildScrollView(
-          child: Column(children: [
+      // resizeToAvoidBottomInset: false,
+      backgroundColor: Colors.deepPurple[50],
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
             Container(
               padding: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.0001),
+              // margin: EdgeInsets.only(
+              //     top: MediaQuery.of(context).size.height * 0.0001),
               width: double.infinity,
               height: 200,
               decoration: BoxDecoration(
@@ -175,90 +177,97 @@ class _SeatSelectPageState extends State<SeatSelectPage> {
                 ),
               ],
             ),
-            SizedBox(
-              height: 30,
-            ),
+            SizedBox(height: 30),
             Column(
               children: _buildSeatGrid(),
             ),
-            Container(
-              padding: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
-              margin: EdgeInsets.only(
-                  top: MediaQuery.of(context).size.height * 0.05),
-              width: double.infinity,
-              // height: 220,
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                    topRight: Radius.elliptical(20, 20),
-                    topLeft: Radius.elliptical(20, 20)),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        'Seat No: ',
-                        style: TextStyle(
+            SizedBox(height: 20),
+            Flexible(
+              child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+                // margin: EdgeInsets.only(
+                // top: MediaQuery.of(context).size.height * 0.05),
+                width: double.infinity,
+                // height: 220,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.elliptical(20, 20),
+                      topLeft: Radius.elliptical(20, 20)),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          'Seat No: ',
+                          style: TextStyle(
                             fontSize: 23,
                             fontWeight: FontWeight.w500,
-                            color: Colors.deepPurple[300]),
-                      ),
-                      Expanded(
-                        child: Text(
-                          ' ${selectedSeats.join(', ')}',
-                          style: TextStyle(
-                              color: Colors.deepPurple[300], fontSize: 20),
+                            color: Colors.deepPurple[300],
+                          ),
                         ),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    'Price: Ghc ${totalPrice.toStringAsFixed(2)}',
-                    style: TextStyle(
+                        Expanded(
+                          child: Text(
+                            ' ${selectedSeats.join(', ')}',
+                            style: TextStyle(
+                              color: Colors.deepPurple[300],
+                              fontSize: 20,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                    Text(
+                      'Price: Ghc ${totalPrice.toStringAsFixed(2)}',
+                      style: TextStyle(
                         fontSize: 23,
                         fontWeight: FontWeight.w500,
-                        color: Colors.deepPurple[300]),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                                builder: (context) => PassengerDetailsPage(
-                                      totalPrice: totalPrice,
-                                      selectedSeats: selectedSeats,
-                                    )));
-                      },
-                      style: TextButton.styleFrom(
-                        backgroundColor: Color.fromRGBO(19, 41, 75, 1),
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 60, vertical: 15),
-                        shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(25)),
-                      ),
-                      child: Text(
-                        'Continue',
-                        style: TextStyle(
-                            color: Colors.white,
-                            letterSpacing: 1.5,
-                            fontSize: 15),
+                        color: Colors.deepPurple[300],
                       ),
                     ),
-                  ),
-                ],
+                    SizedBox(height: 15),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => PassengerDetailsPage(
+                                totalPrice: totalPrice,
+                                selectedSeats: selectedSeats,
+                              ),
+                            ),
+                          );
+                        },
+                        style: TextButton.styleFrom(
+                          backgroundColor: Color.fromRGBO(19, 41, 75, 1),
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 60, vertical: 15),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(25),
+                          ),
+                        ),
+                        child: Text(
+                          'Continue',
+                          style: TextStyle(
+                            color: Colors.white,
+                            letterSpacing: 1.5,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ]),
-        ));
+          ],
+        ),
+      ),
+    );
   }
 }
