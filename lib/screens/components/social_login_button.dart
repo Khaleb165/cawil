2a04edae2ea05cc26e34e7e33a58795a76540750
Khaleb165 <calebagyei165@gmail.com>
2024-02-12 13:54:35 +0000

    create widget for social media login

---
+++ lib/screens/components/social_login_button.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../../constants/colors.dart';
+
+class SocialLoginButton extends StatelessWidget {
+  final String imageUrl;
+  const SocialLoginButton({super.key, required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      radius: 25,
+      backgroundColor: colorWhite,
+      child: Padding(
+        padding: const EdgeInsets.all(12), // Border radius
+        child: Image.asset(imageUrl),
+      ),
+    );
+  }
+}
