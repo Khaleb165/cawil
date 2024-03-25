07de68a04a3737a0525c32e62ab9e735e36d4e84
Khaleb165 <calebagyei165@gmail.com>
2024-03-25 15:37:02 +0000

    make changes to UI, code refactoring

--- lib/screens/payment/payment_details.dart
+++ lib/screens/payment/payment_details.dart
@@ -1,16 +1,15 @@
+import 'package:cawil/constants/colors.dart';
 import 'package:cawil/screens/payment/payment_succes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-import '../../models/bus_data.dart';
+import '../../providers/bus_data.dart';
 
 class PaymentDetailsPage extends StatelessWidget {
-
   final String paymentMethod;
 
   const PaymentDetailsPage({
     super.key,
-
     required this.paymentMethod,
   });
 
@@ -30,81 +29,75 @@ class PaymentDetailsPage extends StatelessWidget {
     return Scaffold(
       backgroundColor: Colors.deepPurple[50],
       body: SingleChildScrollView(
-          child: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
-            margin: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.0001),
-            width: double.infinity,
-            height: 200,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                colors: [
-                  const Color.fromRGBO(0, 7, 240, 0.5),
-                  Color.fromRGBO(0, 7, 240, 0.5),
-                  Color.fromRGBO(127, 0, 255, 100)
-                ],
-                tileMode: TileMode.clamp,
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+              width: double.infinity,
+              height: 200,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [shade1, shade1, shade2],
+                  tileMode: TileMode.clamp,
+                ),
+                borderRadius: BorderRadius.vertical(
+                  bottom: Radius.elliptical(50, 50),
+                ),
               ),
-              borderRadius: BorderRadius.only(
-                  bottomRight: Radius.elliptical(50, 50),
-                  bottomLeft: Radius.elliptical(50, 50)),
-            ),
-            child: Align(
-              alignment: Alignment.center,
-              child: Text(
-                'Enter Payment Details',
-                style: TextStyle(
-                    color: Colors.white, fontSize: 25, letterSpacing: 1),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Enter Payment Details',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 25,
+                  ),
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 150,
-          ),
-          Padding(
-            padding: const EdgeInsets.only(left: 15, right: 15),
-            child: SizedBox(
+            SizedBox(height: 100),
+            Container(
+              padding: const EdgeInsets.only(left: 15, right: 15),
               child: Card(
                 child: Padding(
-                  padding: EdgeInsets.only(
-                    left: 10,
-                    right: 20,
-                  ),
+                  padding: EdgeInsets.only(left: 10, right: 20),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      SizedBox(
-                        height: 25,
-                      ),
+                      SizedBox(height: 25),
                       Text(
                         'Payment Method',
                         style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 15,
-                            color: Colors.black45),
+                          fontWeight: FontWeight.normal,
+                          fontSize: 15,
+                          color: Colors.black45,
+                        ),
                       ),
-                      SizedBox(
-                        height: 5,
+                      SizedBox(height: 5),
+                      Text(
+                        '$paymentMethod'.toUpperCase(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        ),
                       ),
-                      Text('$paymentMethod'.toUpperCase(),
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 18)),
                       SizedBox(height: 16.0),
                       Text(
-                        'Mobile Number: ${Provider.of<BusData>(context).phoneNumber}',
+                        'Mobile Number',
                         style: TextStyle(
-                            fontWeight: FontWeight.normal,
-                            fontSize: 15,
-                            color: Colors.black45),
+                          fontWeight: FontWeight.normal,
+                          fontSize: 15,
+                          color: Colors.black45,
+                        ),
                       ),
-                      SizedBox(
-                        height: 5,
+                      SizedBox(height: 5),
+                      Text(
+                        '${Provider.of<BusData>(context).phoneNumber}',
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 18,
+                        ),
                       ),
-                      Text('${Provider.of<BusData>(context).phoneNumber}',
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500, fontSize: 18)),
                       SizedBox(height: 20.0),
                       Row(
                         children: [
@@ -114,18 +107,19 @@ class PaymentDetailsPage extends StatelessWidget {
                               Text(
                                 'Ticket Price',
                                 style: TextStyle(
-                                    fontWeight: FontWeight.normal,
-                                    fontSize: 15,
-                                    color: Colors.black45),
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 15,
+                                  color: Colors.black45,
+                                ),
                               ),
-                              SizedBox(
-                                height: 5
-                              ),
+                              SizedBox(height: 5),
                               Text(
-                                  'Ghc ${Provider.of<BusData>(context).totalPrice.toStringAsFixed(2)}',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 18))
+                                'Ghc ${Provider.of<BusData>(context).totalPrice.toStringAsFixed(2)}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                ),
+                              ),
                             ],
                           ),
                           Spacer(),
@@ -135,64 +129,60 @@ class PaymentDetailsPage extends StatelessWidget {
                               Text(
                                 'E-levy Charges',
                                 style: TextStyle(
-                                    fontWeight: FontWeight.normal,
-                                    fontSize: 15,
-                                    color: Colors.black45),
+                                  fontWeight: FontWeight.normal,
+                                  fontSize: 15,
+                                  color: Colors.black45,
+                                ),
                               ),
-                              SizedBox(
-                                height: 5
+                              SizedBox(height: 5),
+                              Text(
+                                'Ghc ${charges.toStringAsFixed(2)}',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 18,
+                                ),
                               ),
-                              Text('Ghc ${charges.toStringAsFixed(2)}',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w500,
-                                      fontSize: 18)),
                             ],
-                          )
+                          ),
                         ],
                       ),
-                      SizedBox(
-                        height: 30
-                      )
+                      SizedBox(height: 30)
                     ],
                   ),
                 ),
               ),
             ),
-          ),
-          SizedBox(
-            height: 40,
-          ),
-          SizedBox(
-            height: 55,
-            width: 350,
-            child: ElevatedButton(
-              onPressed: (() {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => PaymentSuccessPage(
-
-                            )));
-              }),
-              style: TextButton.styleFrom(
-                  disabledBackgroundColor: Colors.greenAccent[100],
-                  backgroundColor: Colors.greenAccent,
-                  padding: EdgeInsets.symmetric(horizontal: 90, vertical: 15),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(25)),
-                  elevation: 0),
-              child: Text(
-                'Confirm',
-                style: TextStyle(
+            SizedBox(height: 40),
+            SizedBox(
+              height: 55,
+              width: 350,
+              child: ElevatedButton(
+                onPressed: (() {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => PaymentSuccessPage()));
+                }),
+                style: TextButton.styleFrom(
+                    disabledBackgroundColor: Colors.greenAccent[100],
+                    backgroundColor: Colors.greenAccent,
+                    padding: EdgeInsets.symmetric(horizontal: 90, vertical: 15),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(25)),
+                    ),
+                child: Text(
+                  'Confirm',
+                  style: TextStyle(
                     color: Colors.white,
-                    letterSpacing: 1,
                     fontSize: 16,
-                    fontWeight: FontWeight.w600),
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
               ),
             ),
-          )
-        ],
-      )),
+          ],
+        ),
+      ),
     );
   }
 }
