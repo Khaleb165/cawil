55331c1b410ffcffcc17ba485591cf61eb2aaa1c
Khaleb165 <calebagyei165@gmail.com>
2024-03-25 16:09:04 +0000

    changes to snack bar

--- lib/utilities/utils.dart
+++ lib/utilities/utils.dart
@@ -3,16 +3,31 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
-pickImage(ImageSource source) async{
+import '../constants/colors.dart';
+
+pickImage(ImageSource source) async {
   final ImagePicker _imagePicker = ImagePicker();
 
   XFile? _file = await _imagePicker.pickImage(source: source);
-  if(_file != null){
+  if (_file != null) {
     return await _file.readAsBytes();
   }
   print('No image selected');
 }
 
-showSnackBar(String content, BuildContext context){
-  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(content)));
+showSnackBar(String content, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      backgroundColor: primary1,
+      behavior: SnackBarBehavior.floating,
+      dismissDirection: DismissDirection.startToEnd,
+      content: Text(
+        content,
+        style: TextStyle(
+          color: colorWhite,
+          fontSize: 16,
+        ),
+      ),
+    ),
+  );
 }
