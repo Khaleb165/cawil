723db84fc76f467f139c7bdaf4f96d0b4fdab5c7
Khaleb165 <calebagyei165@gmail.com>
2024-03-25 16:09:39 +0000

    create widget directory and move widgets there

---
+++ lib/widgets/app_name.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+class AppName extends StatelessWidget {
+  final double fontSize;
+  const AppName({
+    super.key, required this.fontSize,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'Ca',
+          style: TextStyle(
+              fontSize: fontSize,
+              color: primary1,
+              fontWeight: FontWeight.bold),
+        ),
+        Text(
+          'Wil',
+          style: TextStyle(
+            fontSize: fontSize,
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(19, 41, 75, 1),
+          ),
+        ),
+      ],
+    );
+  }
+}
