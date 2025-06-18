7e0ee0351dbe76e9247ed32ded4978c6be1fd11b
cagyei-quantum <cagyei@quantumgroupgh.com>
2025-06-18 16:12:49 +0000

    add new colors to colors file

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 
-Color greenAccentColor = Colors.greenAccent;
+Color backgroundColor = const Color(0xFFEDE7F6);
+Color greenAccentColor = const Color(0xFF69F0AE);
+Color lightGreenColor = const Color(0xFFB9F6CA);
 Color darkBlueColor = const Color(0xFF13294B);
 Color deepBlueColor = const Color(0x7E0007F0);
 Color purpleColor = const Color(0x9C7F00FF);
 Color whiteColor = const Color(0xFFFFFFFF);
+Color lightWhiteColor = const Color(0xFFFDFBFF);
+Color blackColor = const Color(0xFF000000);
+Color lightBlackColor = const Color(0x60000000);
+Color deepRedColor = const Color(0xFFB71C1C);
