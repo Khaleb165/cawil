4e43fc25f277ebcb84476a7aa110f5e1efb0e006
cagyei-quantum <cagyei@quantumgroupgh.com>
2025-07-04 17:23:00 +0000

    add responsiveness to screen, code refactoring

--- lib/screens/available_bus_page.dart
+++ lib/screens/available_bus_page.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: unused_import
 
 import 'package:cawil/constants/colors.dart';
+import 'package:cawil/constants/size_config.dart';
 import 'package:cawil/widgets/app_name.dart';
 import 'package:cawil/screens/components/TicketCard.dart';
 import 'package:cawil/screens/seat_select.dart';
@@ -17,43 +18,47 @@ class AvailableBusPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ScreenSize().init(context);
     return Scaffold(
-      backgroundColor: Colors.deepPurple[50],
+      backgroundColor: backgroundColor,
       body: Column(
         children: [
           Container(
-            padding: const EdgeInsets.symmetric(horizontal: 35, vertical: 20),
+            padding: EdgeInsets.symmetric(
+                horizontal: getProportionateScreenWidth(30),
+                vertical: getProportionateScreenHeight(20)),
             width: double.infinity,
-            height: 200,
+            height: getProportionateScreenHeight(200),
             decoration: BoxDecoration(
               gradient: LinearGradient(
                 colors: [deepBlueColor, deepBlueColor, purpleColor],
                 tileMode: TileMode.clamp,
               ),
-              borderRadius: const BorderRadius.vertical(
-                  bottom: Radius.elliptical(50, 50)),
+              borderRadius: BorderRadius.vertical(
+                  bottom: Radius.elliptical(getProportionateScreenWidth(50),
+                      getProportionateScreenHeight(40))),
             ),
-            child: const AppName(fontSize: 50),
+            child: AppName(fontSize: getProportionateScreenHeight(50)),
           ),
-          const SizedBox(height: 10),
+          SizedBox(height: getProportionateScreenHeight(10)),
           Column(
             children: [
               Image.asset(
                 'assets/bus-logo.png',
-                scale: 4,
+                scale: getProportionateScreenHeight(5),
               ),
-              const SizedBox(height: 10),
+              SizedBox(height: getProportionateScreenHeight(10)),
               Text(
                 'Buses Available',
                 style: TextStyle(
-                  fontSize: 35,
+                  fontSize: getProportionateScreenHeight(30),
                   color: darkBlueColor,
                   fontWeight: FontWeight.bold,
                 ),
               ),
             ],
           ),
-          const SizedBox(height: 25),
+          SizedBox(height: getProportionateScreenHeight(20)),
           Expanded(
             child: ListView.builder(
               itemCount: 3,
