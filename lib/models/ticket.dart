ff0288b248c07a20da4eb3c0ab0b71f640c1c81e
cagyei-quantum <cagyei@quantumgroupgh.com>
2026-06-18 11:38:58 +0000

    feat: add TicketModel class to represent ticket details

---
+++ lib/models/ticket.dart
@@ -0,0 +1,19 @@
+class TicketModel {
+  final String? busnumber;
+  final String? start_location;
+  final String? Destination;
+  final String? report_time;
+  final String? departure_time;
+  final String? seats_left;
+  final String? price;
+
+  TicketModel({
+    this.busnumber,
+    this.start_location,
+    this.Destination,
+    this.report_time,
+    this.departure_time,
+    this.seats_left,
+    this.price,
+  });
+}
