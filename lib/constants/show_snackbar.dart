6db2205cb93fea9586ed5277e2cd9168581e6148
cagyei-quantum <cagyei@quantumgroupgh.com>
2026-06-25 14:09:30 +0000

    chore: move show snackbar function to new file

---
+++ lib/constants/show_snackbar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import '../constants/colors.dart';
+
+void showSnackBar(String content, BuildContext context) {
+  ScaffoldMessenger.of(context).showSnackBar(
+    SnackBar(
+      backgroundColor: greenAccentColor,
+      behavior: SnackBarBehavior.floating,
+      dismissDirection: DismissDirection.startToEnd,
+      content: Text(
+        content,
+        style: TextStyle(
+          color: whiteColor,
+          fontSize: 16,
+        ),
+      ),
+    ),
+  );
+}
