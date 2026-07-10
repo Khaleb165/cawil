618ad92be8495c87362267e3bf72cf19863f24bd
cagyei-quantum <cagyei@quantumgroupgh.com>
2026-07-10 17:44:39 +0000

    chore: Initialize app services in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:cawil/core/constants/colors.dart';
+import 'package:cawil/data/offline/hive_storage.dart';
+import 'package:cawil/data/remote/dio_client.dart';
 import 'package:cawil/view_model/bus_data.dart';
 import 'package:cawil/data/resources/auth_methods.dart';
 import 'package:cawil/view/screens/homepage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 
 import 'view/screens/intro_screen.dart';
@@ -13,10 +14,8 @@ import 'view/screens/intro_screen.dart';
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
-
-  await Hive.initFlutter();
-  await Hive.openBox<dynamic>('auth');
-  await Hive.openBox<dynamic>('user_profile');
+  await HiveStorage.init();
+  await DioClient().initDioClient();
 
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
