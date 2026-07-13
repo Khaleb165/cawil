2a68c1bb1cb52c80d53dd79688558f6fd7de797d
cagyei-quantum <cagyei@quantumgroupgh.com>
2026-07-13 17:53:05 +0000

    feat: Enhance AvailableBusModel for schedule search results

--- lib/model/available_bus.dart
+++ lib/model/available_bus.dart
@@ -1,4 +1,7 @@
+import 'package:intl/intl.dart';
+
 class AvailableBusModel {
+  int? scheduleId;
   String? busNumber;
   String? reportTime;
   String? departureTime;
@@ -7,6 +10,7 @@ class AvailableBusModel {
   double? price;
 
   AvailableBusModel({
+    this.scheduleId,
     this.busNumber,
     this.reportTime,
     this.departureTime,
@@ -14,4 +18,32 @@ class AvailableBusModel {
     this.seatsLeft,
     this.price,
   });
+
+  factory AvailableBusModel.fromScheduleJson(Map<String, dynamic> json) {
+    final schedule = Map<String, dynamic>.from(json['schedule'] as Map);
+    final busInfo = Map<String, dynamic>.from(json['bus_info'] as Map);
+    final departure = _parseDateTime(schedule['departure_time']);
+    final arrival = _parseDateTime(schedule['arrival_time']);
+    final report = _parseDateTime(schedule['report_time']);
+
+    return AvailableBusModel(
+      scheduleId: schedule['id'] as int?,
+      busNumber: busInfo['bus_number']?.toString(),
+      reportTime: _formatTime(report),
+      departureTime: _formatTime(departure),
+      arrivalTime: _formatTime(arrival),
+      seatsLeft: (schedule['seats_remaining'] as num?)?.toInt(),
+      price: (schedule['price'] as num?)?.toDouble(),
+    );
+  }
+
+  static DateTime? _parseDateTime(Object? value) {
+    if (value == null) return null;
+    return DateTime.tryParse(value.toString())?.toLocal();
+  }
+
+  static String _formatTime(DateTime? value) {
+    if (value == null) return 'N/A';
+    return DateFormat('hh:mm a').format(value);
+  }
 }
