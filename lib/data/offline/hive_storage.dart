2290ed9ea069161365117124d05c4983efe81919
cagyei-quantum <cagyei@quantumgroupgh.com>
2026-07-13 17:53:05 +0000

    chore: Add debug print for user details in HiveStorage

--- lib/data/offline/hive_storage.dart
+++ lib/data/offline/hive_storage.dart
@@ -1,4 +1,5 @@
 import 'package:cawil/model/user.dart';
+import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
 class HiveStorage {
@@ -66,6 +67,7 @@ class HiveStorage {
     final user = User.fromJson(
       Map<String, dynamic>.from(data[_userKey] as Map),
     );
+    debugPrint('User details ---- $user');
     await cacheUser(user);
   }
 
