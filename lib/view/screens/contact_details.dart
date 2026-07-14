250b03086320a0760218092f223466b7899cddac
cagyei-quantum <cagyei@quantumgroupgh.com>
2026-07-14 14:28:15 +0000

    feat: Add ContactDetailsPage for user contact information input

---
+++ lib/view/screens/contact_details.dart
@@ -0,0 +1,168 @@
+import 'package:cawil/core/constants/colors.dart';
+import 'package:cawil/view_model/bus_data.dart';
+import 'package:cawil/view/widgets/custom_textfield.dart';
+import 'package:cawil/view/screens/payment/payment_method.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../core/constants/size_config.dart';
+
+class ContactDetailsPage extends StatefulWidget {
+  const ContactDetailsPage({Key? key}) : super(key: key);
+
+  @override
+  State<ContactDetailsPage> createState() => _ContactDetailsPageState();
+}
+
+class _ContactDetailsPageState extends State<ContactDetailsPage> {
+  TextEditingController contactController = TextEditingController();
+  TextEditingController contactNumberController = TextEditingController();
+  String? selectedGender;
+
+  @override
+  Widget build(BuildContext context) {
+    ScreenSize().init(context);
+    return Scaffold(
+      backgroundColor: backgroundColor,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(
+                  horizontal: getProportionateScreenWidth(30),
+                  vertical: getProportionateScreenHeight(20)),
+              width: double.infinity,
+              height: getProportionateScreenHeight(200),
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [deepBlueColor, deepBlueColor, purpleColor],
+                  tileMode: TileMode.clamp,
+                ),
+                borderRadius: const BorderRadius.vertical(
+                  bottom: Radius.elliptical(50, 50),
+                ),
+              ),
+              child: Align(
+                alignment: Alignment.center,
+                child: Text(
+                  'Contact Details',
+                  style: TextStyle(
+                    color: whiteColor,
+                    fontSize: getProportionateScreenHeight(30),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: getProportionateScreenHeight(90)),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                  horizontal: getProportionateScreenWidth(10)),
+              child: Column(
+                children: [
+                  CustomTextfield(
+                    controller: contactController,
+                    hintText: 'Contact Person',
+                    onChanged: (newText) {
+                      Provider.of<BusData>(context, listen: false)
+                          .updateNameTextField(newText);
+                    },
+                  ),
+                  SizedBox(height: getProportionateScreenHeight(15)),
+                  CustomTextfield(
+                    controller: contactNumberController,
+                    keyboardType: TextInputType.phone,
+                    hintText: 'Phone Number',
+                    onChanged: (newNumber) {
+                      Provider.of<BusData>(context, listen: false)
+                          .updatePhoneTextField(newNumber);
+                    },
+                  ),
+                  SizedBox(height: getProportionateScreenHeight(15)),
+                  DropdownButtonFormField<String>(
+                    initialValue: selectedGender,
+                    icon: const Icon(Icons.arrow_drop_down),
+                    elevation: 16,
+                    style: TextStyle(color: lightBlackColor),
+                    decoration: InputDecoration(
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: whiteColor, width: 0.0),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      hintText: 'Gender',
+                      hintStyle: TextStyle(color: lightBlackColor),
+                      filled: true,
+                      floatingLabelBehavior: FloatingLabelBehavior.never,
+                      fillColor: whiteColor,
+                      contentPadding: EdgeInsets.symmetric(
+                          horizontal: getProportionateScreenWidth(20),
+                          vertical: getProportionateScreenHeight(10)),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(20),
+                        borderSide: BorderSide(
+                          style: BorderStyle.solid,
+                          color: lightBlackColor,
+                        ),
+                      ),
+                    ),
+                    items: <String>['Male', 'Female']
+                        .map<DropdownMenuItem<String>>((String value) {
+                      return DropdownMenuItem<String>(
+                        value: value,
+                        child: Text(value),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        selectedGender = newValue;
+                      });
+                    },
+                  ),
+                  SizedBox(height: getProportionateScreenHeight(15)),
+                  SizedBox(height: getProportionateScreenHeight(40)),
+                  Center(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        if (contactController.text.trim().isEmpty ||
+                            contactNumberController.text.trim().isEmpty) {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(
+                                content: Text(
+                                    'Name of traveller and Phone Number are required.')),
+                          );
+                          return;
+                        }
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const PaymentMethodPage(),
+                          ),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: greenAccentColor,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: getProportionateScreenWidth(90),
+                            vertical: getProportionateScreenHeight(15)),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(25),
+                        ),
+                      ),
+                      child: Text(
+                        'Proceed to Payment',
+                        style: TextStyle(
+                          color: whiteColor,
+                          fontSize: getProportionateScreenHeight(15),
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
