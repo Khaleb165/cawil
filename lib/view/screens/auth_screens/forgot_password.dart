39b9fceaf97342e988e32d6424203e46c05a0037
cagyei-quantum <cagyei@quantumgroupgh.com>
2026-07-24 14:57:58 +0000

    feat: Update ForgotPasswordPage layout and integrate CustomButton for reset action

--- lib/view/screens/auth_screens/forgot_password.dart
+++ lib/view/screens/auth_screens/forgot_password.dart
@@ -1,6 +1,7 @@
 import 'package:cawil/core/constants/colors.dart';
 import 'package:cawil/core/constants/size_config.dart';
 import 'package:cawil/view/widgets/app_name.dart';
+import 'package:cawil/view/widgets/custom_button.dart';
 import 'package:cawil/view/widgets/custom_textfield.dart';
 import 'package:flutter/material.dart';
 
@@ -12,6 +13,7 @@ class ForgotPasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TextEditingController _emailTextController = TextEditingController();
+    final bool _isLoading = false;
     return Scaffold(
       body: Container(
         height: double.infinity,
@@ -21,68 +23,67 @@ class ForgotPasswordPage extends StatelessWidget {
             tileMode: TileMode.clamp,
           ),
         ),
-        child: SingleChildScrollView(
+        child: Padding(
           padding: EdgeInsets.symmetric(
-              horizontal: getProportionateScreenWidth(25),
-              vertical: getProportionateScreenHeight(50)),
+            horizontal: getProportionateScreenWidth(25),
+            vertical: getProportionateScreenHeight(50),
+          ),
           child: Column(
             crossAxisAlignment: crossCenter,
             children: [
-              const AppName(fontSize: 70),
-              SizedBox(height: getProportionateScreenHeight(150)),
-              Text(
-                'Enter your e-mail and we will send you a reset link',
-                style: TextStyle(
-                  color: whiteColor,
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              SizedBox(height: getProportionateScreenHeight(40)),
-              CustomTextfield(
-                controller: _emailTextController,
-                keyboardType: TextInputType.emailAddress,
-                inputAction: TextInputAction.done,
-                hintText: 'Email Address',
-                onSubmitted: () {
-                  // Handle the submission of the email address
-                },
-              ),
-              SizedBox(height: getProportionateScreenHeight(25)),
-              Center(
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: TextButton.styleFrom(
-                    backgroundColor: lightGreenColor,
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 130, vertical: 18),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(25)),
-                  ),
-                  child: Text(
-                    'RESET',
-                    style: TextStyle(
-                      color: whiteColor,
-                    ),
-                  ),
-                ),
-              ),
-              SizedBox(height: getProportionateScreenHeight(20)),
-              Align(
-                alignment: Alignment.topRight,
-                child: TextButton(
-                  onPressed: () {
-                    Navigator.pop(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => const LoginScreen()));
-                  },
-                  child: Text(
-                    'Back',
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: lightGreenColor,
-                    ),
+              AppName(fontSize: getProportionateScreenHeight(50)),
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: crossCenter,
+                    children: [
+                      SizedBox(height: getProportionateScreenHeight(150)),
+                      Text(
+                        'Enter your e-mail for a reset link',
+                        style: TextStyle(
+                          color: whiteColor,
+                          fontSize: getProportionateScreenHeight(15),
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
+                      SizedBox(height: getProportionateScreenHeight(40)),
+                      CustomTextfield(
+                        controller: _emailTextController,
+                        keyboardType: TextInputType.emailAddress,
+                        inputAction: TextInputAction.done,
+                        hintText: 'Email Address',
+                        onSubmitted: () {
+                          // Handle the submission of the email address
+                        },
+                      ),
+                      SizedBox(height: getProportionateScreenHeight(25)),
+                      CustomButton(
+                        onPressed: () {
+                          // Handle the reset password action
+                        },
+                        text: 'RESET',
+                        isLoading: _isLoading,
+                      ),
+                      SizedBox(height: getProportionateScreenHeight(20)),
+                      Align(
+                        alignment: Alignment.topRight,
+                        child: TextButton(
+                          onPressed: () {
+                            Navigator.pop(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const LoginScreen()));
+                          },
+                          child: Text(
+                            'Back',
+                            style: TextStyle(
+                              fontSize: getProportionateScreenHeight(15),
+                              color: lightGreenColor,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
               ),
